eaa8985074e533721e536c25c831929ac8eff77d
Zpf-GuiDou <56754373+Zpf-GuiDou@users.noreply.github.com>
2020-04-05 20:04:04 +0800

    push

--- js/yiqi.js
+++ js/yiqi.js
@@ -1,17 +1,18 @@
 ﻿// JavaScript Document
 var Dianji=0;
+var name = prompt("请输入姓名");
 window.onload=function(){
 	var buhao = document.getElementById("buhao");
 	var hao = document.getElementById("hao");
 	buhao.onclick=function(){
 		Dianji++;
 		if(Dianji==1){
-	   			alert("小姐姐在考虑一下呗");
+	   			alert(name+"在考虑一下呗");
 	   }else if(Dianji==2){
-		   		alert("你是我见过的最漂亮的女孩子");
+		   		alert("你是我见过的最漂亮的女生");
 		   		
 	   }else if(Dianji==3){
-		   		alert("一生一世爱你");
+		   		alert("一生一世只爱爱"+name);
 		   		
 	   }else if(Dianji==4){
 		   		alert("家务我全干");
@@ -24,10 +25,9 @@ window.onload=function(){
 		   		alert("工资全上交");
 		  		Dianji=1;
 	  }
-
 	}
 	hao.onclick=function(){
-		alert("小姐姐终于同意了");
+		alert(name+"你终于同意了");
 	}
 	
 
